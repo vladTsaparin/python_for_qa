0518c41467aca50deccc4af6651a78056174b7bf
DS_VladTS <vladislav.tsaparin@djangostars.com>
2021-12-12 14:45:09 +0200

    delete unusing def

--- test_add_group.py
+++ test_add_group.py
@@ -36,21 +36,10 @@ class AppDynamicsJob(unittest.TestCase):
         except NoAlertPresentException as e: return False
         return True
     
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.wd.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally: self.accept_next_alert = True
-    
     def tearDown(self):
         # To know more about the difference between verify and assert,
         # visit https://www.seleniumhq.org/docs/06_test_design_considerations.jsp#validating-results
-        self.assertEqual([], self.verificationErrors)
+        self.driver.quit()
 
 if __name__ == "__main__":
     unittest.main()
