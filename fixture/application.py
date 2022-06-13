cb76c747819cb5defa228fa784e6c34ab541132e
DS_VladTS <vladislav.tsaparin@djangostars.com>
2022-06-13 18:53:35 +0300

    added open_registration_page function

--- fixture/application.py
+++ fixture/application.py
@@ -11,10 +11,15 @@ class Application:
         self.wd.implicitly_wait(60)
         self.session = SessionHelper(self)
 
+    def open_home_page(self, domain):
+        wd = self.wd
+        wd.get(domain)
+        self.cookies_accept()
+
     def open_login_page(self):
         wd = self.wd
-        wd.get("https://jysk.ua/customer/login")
-        self.cookies_accept()
+        self.open_home_page('https://jysk.ua/')
+        wd.find_element(By.XPATH, '//a[@href="/customer/login"]').click()
 
     def cookies_accept(self):
         wd = self.wd
@@ -22,8 +27,8 @@ class Application:
 
     def open_registration_page(self):
         wd = self.wd
-        wd.get("https://jysk.ua/customer/create")
-        self.cookies_accept()
+        self.open_login_page()
+        wd.find_element(By.XPATH, '//a[@href="/customer/create"]').click()
 
     def destroy(self):
         self.wd.quit()
--- test/test_registration.py
+++ test/test_registration.py
@@ -13,5 +13,19 @@ def app(request):
     return fixture
 
 
-def test_registration_valid(app):
-    app.session.registration(User(email=f"for.qa14+{datetime.now().microsecond}@gmail.com", password="QAtl00584", first_name="first_name", last_name="last_name"))
+def test_registration_new_user(app):
+    app.session.registration(User(
+        email=f"for.qa14+{datetime.now().microsecond}@gmail.com",
+        password="QAtl00584",
+        first_name="перший",
+        last_name="другий"
+    ))
+
+
+def test_registration_with_existing_email(app):
+    app.session.registration(User(
+        email=f"for.qa14@gmail.com",
+        password="QAtl00584",
+        first_name="перший",
+        last_name="другий"
+    ))
